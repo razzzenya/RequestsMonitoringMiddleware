17817f626db2dc9b8c17d5fafa9f96f183375b97
Ivan K <buckingkiska@gmail.com>
2026-02-22 17:24:11 +0400

    добавил дашборды opensearch в Apphost

--- RequestMonitoring.AppHost/AppHost.cs
+++ RequestMonitoring.AppHost/AppHost.cs
@@ -9,6 +9,12 @@ var openSearch = builder.AddContainer("opensearch", "opensearchproject/opensearc
     .WithEnvironment("OPENSEARCH_INITIAL_ADMIN_PASSWORD", "Admin@123")
     .WithEnvironment("DISABLE_SECURITY_PLUGIN", "true");
 
+var openSearchDashboard = builder.AddContainer("opensearch-dashboard", "opensearchproject/opensearch-dashboards", "2.12.0")
+    .WithHttpEndpoint(port: 5601, targetPort: 5601, name: "http")
+    .WithEnvironment("OPENSEARCH_HOSTS", "http://opensearch:9200")
+    .WithEnvironment("DISABLE_SECURITY_DASHBOARDS_PLUGIN", "true")
+    .WaitFor(openSearch);
+
 var api = builder.AddProject<Projects.RequestMonitoring_Test_Api>("api")
     .WaitFor(openSearch)
     .WaitFor(redis)
