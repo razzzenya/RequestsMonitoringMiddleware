567bee68c33be61d7dd8f0142bb1349f37439bda
Ivan K <buckingkiska@gmail.com>
2026-03-16 20:25:28 +0400

    Обёртка для api

---
+++ RequestMonitoring.AdminPanel/Api/IRequestMonitoringAdminPanelApiWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace RequestMonitoring.AdminPanel.Api;
+
+public interface IRequestMonitoringAdminPanelApiWrapper
+{
+    public Task<IEnumerable<Domain>> GetDomainList();
+    public Task<Domain> GetDomain(int id);
+    public Task<Domain> CreateDomain(CreateDomainDTO dto);
+    public Task<Domain> UpdateDomain(int id, UpdateDomainDTO dto);
+    public Task DeleteDomain(int id);
+}
---
+++ RequestMonitoring.AdminPanel/Api/RequestMonitoringAdminPanelApiWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace RequestMonitoring.AdminPanel.Api;
+
+public class RequestMonitoringAdminPanelApiWrapper(IConfiguration configuration) : IRequestMonitoringAdminPanelApiWrapper
+{
+    public readonly RequestMonitoringAdminPanelApi _client = new(configuration["OpenApi:ServerUrl"], new HttpClient());
+
+    public async Task<Domain> CreateDomain(CreateDomainDTO dto) => await _client.DomainsPOSTAsync(dto);
+
+    public async Task DeleteDomain(int id) => await _client.DomainsDELETEAsync(id);
+
+    public async Task<Domain> GetDomain(int id) => await _client.DomainsGETAsync(id);
+
+    public async Task<IEnumerable<Domain>> GetDomainList() => await _client.DomainsAllAsync();
+
+    public async Task<Domain> UpdateDomain(int id, UpdateDomainDTO dto) => await _client.DomainsPUTAsync(id, dto);
+}
