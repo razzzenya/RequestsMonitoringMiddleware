673f10fdc2de1e980894f92e7c8907df4cfd17c3
Ivan K <buckingkiska@gmail.com>
2026-04-19 10:50:35 +0400

    Фронт для создания квот

---
+++ RequestMonitoring.AdminPanel/Models/QuotaType.cs
@@ -0,0 +1,11 @@
+namespace RequestMonitoring.AdminPanel.Models;
+
+public enum QuotaType
+{
+    Unlimited,
+    Periodic,
+    Total,
+    ExpiringUnlimited,
+    ExpiringTotal,
+    ExpiringPeriodic
+}
---
+++ RequestMonitoring.AdminPanel/Models/QuotaTypeHelper.cs
@@ -0,0 +1,26 @@
+namespace RequestMonitoring.AdminPanel.Models;
+
+public static class QuotaTypeHelper
+{
+    public static string GetDisplayName(QuotaType type) => type switch
+    {
+        QuotaType.Unlimited         => "Безлимит",
+        QuotaType.Periodic          => "Периодический",
+        QuotaType.Total             => "Общий",
+        QuotaType.ExpiringUnlimited => "Безлимит до даты",
+        QuotaType.ExpiringTotal     => "Общий до даты",
+        QuotaType.ExpiringPeriodic  => "Периодический до даты",
+        _                           => type.ToString()
+    };
+
+    public static string GetDescription(QuotaType type) => type switch
+    {
+        QuotaType.Unlimited         => "Неограниченное количество запросов без каких-либо ограничений.",
+        QuotaType.Periodic          => "N запросов в период. Счётчик сбрасывается по истечении периода.",
+        QuotaType.Total             => "N запросов всего. После исчерпания домен блокируется.",
+        QuotaType.ExpiringUnlimited => "Безлимит до указанной даты. После истечения домен блокируется.",
+        QuotaType.ExpiringTotal     => "N запросов всего до указанной даты. После исчерпания или истечения домен блокируется.",
+        QuotaType.ExpiringPeriodic  => "N запросов в период до указанной даты. После истечения даты домен блокируется.",
+        _                           => string.Empty
+    };
+}
